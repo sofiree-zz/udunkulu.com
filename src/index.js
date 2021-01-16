d2c2ef3881cf49990868c96a1098b14cba4e7865
sofiree <muosophia0@gmail.com>
2021-01-16 17:13:59 +0100

    FEAT: adjusted index.js and app.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// export {default as Searchbar} from './Reusable/Searchbar.js';
+export {default as Authentication} from './Components/Authentication.js';
 ReactDOM.render(
   <React.StrictMode>
     <App />
