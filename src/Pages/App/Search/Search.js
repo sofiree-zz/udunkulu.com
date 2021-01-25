cf3d3a336381e6a35adf2ecc66d2f785067489c9
sofiree <muosophia0@gmail.com>
2021-01-25 11:31:19 +0100

    FEAT: search bar responsive

--- src/Pages/App/Search/Search.js
+++ src/Pages/App/Search/Search.js
@@ -7,35 +7,35 @@ import { UdunkuluVector } from '../../../Assets/Images';
 
 const Search=()=>{
     return(
-        <div >
            <div class="content">
               
-                    <nav class="navbar m-0" id="nav-bar">
-                        <form action=""
-                            onSubmit={(e) => {
-                                e.preventDefault();
-                            }}
-                        > 
-                            <div class="input-group mb-4  my-3" id="search">
-                                <div class=" border-0">
-                                    <span class="btn"  id="button-addon3"><i class="fa fa-search"></i></span>
-                                </div>
-                                <input type="search" placeholder="Find song..."  class="form-control border-0" size="50" id="input"/>
-                               
+                <nav class="navbar m-0" id="nav-bar">
+                    <form action=""
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                        }}
+                        class="search-form"
+                    > 
+                        <div class="input-group mb-4  my-3" id="search">
+                            <div class=" border-0">
+                                <span class="btn"  id="button-addon3"><i class="fa fa-search"></i></span>
                             </div>
-                        </form>  
-                        <Button variant="sign-in" size={"sm"} data-target={"#authModal"} data-toggle="modal">Sign In</Button>
-                    </nav>
+                            <input type="search" placeholder="Find song..."  class="form-control border-0" size="50" id="input"/>
+                            
+                        </div>
+                    </form>  
+                    <Button variant="sign-in" size={"sm"} data-target={"#authModal"} data-toggle="modal">Sign In</Button>
+                </nav>
 
                     
-                        <div class="d-flex flex-column" id="container">
-                            <img src={UdunkuluVector} alt="Udunkulu" />
-                            <text class="mx-auto" id="text">Contents will appear when you search them</text>
-                        </div>
-                   
+                <div class="d-flex flex-column" id="container">
+                    <img src={UdunkuluVector} alt="Udunkulu" />
+                    <text class="mx-auto" id="text">Contents will appear when you search them</text>
                 </div>
+                   
+            </div>
             
-        </div>
+        
     );
 }
 export {Search};
