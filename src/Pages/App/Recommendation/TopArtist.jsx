364ca528d72aee97acdd8a7f7b19540871b4795b
ifebrand6 <ifebrand6@gmail.com>
2021-01-27 20:31:06 +0100

    implemented Top Artist

--- src/Pages/App/Recommendation/TopArtist.jsx
+++ src/Pages/App/Recommendation/TopArtist.jsx
@@ -1,9 +1,146 @@
-import React from 'react'
+import React from "react";
 
 export  function TopArtist() {
-    return (
-        <div>
-            
+  return (
+    <div>
+    
+      <div className="row pt-3 m-0">
+        <div className="item">
+          <div class="card shadow-sm border-0 rounded genCard">
+            <div class="card-body p-0">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
+                alt=""
+                class="w-100 card-img-top cardImage"
+              />
+              <div class=" albumInnerText">
+                <span class="songName">Made In Lagos</span>
+                <span class="text-muted songCountDetails">Wizkid</span>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+        <div className="item">
+          <div class="card shadow-sm border-0 rounded genCard">
+            <div class="card-body p-0">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
+                alt=""
+                class="w-100 card-img-top cardImage"
+              />
+              <div class=" albumInnerText">
+                <span class="songName">Made In Lagos</span>
+                <span class="text-muted songCountDetails">Wizkid</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <div class="card shadow-sm border-0 rounded genCard">
+            <div class="card-body p-0">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
+                alt=""
+                class="w-100 card-img-top cardImage"
+              />
+              <div class=" albumInnerText">
+                <span class="songName">Made In Lagos</span>
+                <span class="text-muted songCountDetails">Wizkid</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <div class="card shadow-sm border-0 rounded genCard">
+            <div class="card-body p-0">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
+                alt=""
+                class="w-100 card-img-top cardImage"
+              />
+              <div class=" albumInnerText">
+                <span class="songName">Made In Lagos</span>
+                <span class="text-muted songCountDetails">Wizkid</span>
+              </div>
+            </div>
+          </div>
+        </div>
+           <div className="item">
+          <div class="card shadow-sm border-0 rounded genCard">
+            <div class="card-body p-0">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
+                alt=""
+                class="w-100 card-img-top cardImage"
+              />
+              <div class=" albumInnerText">
+                <span class="songName">Made In Lagos</span>
+                <span class="text-muted songCountDetails">Wizkid</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <div class="card shadow-sm border-0 rounded genCard">
+            <div class="card-body p-0">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
+                alt=""
+                class="w-100 card-img-top cardImage"
+              />
+              <div class=" albumInnerText">
+                <span class="songName">Made In Lagos</span>
+                <span class="text-muted songCountDetails">Wizkid</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <div class="card shadow-sm border-0 rounded genCard">
+            <div class="card-body p-0">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
+                alt=""
+                class="w-100 card-img-top cardImage"
+              />
+              <div class=" albumInnerText">
+                <span class="songName">Made In Lagos</span>
+                <span class="text-muted songCountDetails">Wizkid</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <div class="card shadow-sm border-0 rounded genCard">
+            <div class="card-body p-0">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
+                alt=""
+                class="w-100 card-img-top cardImage"
+              />
+              <div class=" albumInnerText">
+                <span class="songName">Made In Lagos</span>
+                <span class="text-muted songCountDetails">Wizkid</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <div class="card shadow-sm border-0 rounded genCard">
+            <div class="card-body p-0">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
+                alt=""
+                class="w-100 card-img-top cardImage"
+              />
+              <div class=" albumInnerText">
+                <span class="songName">Made In Lagos</span>
+                <span class="text-muted songCountDetails">Wizkid</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
