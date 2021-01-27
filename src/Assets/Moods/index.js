bafc6d7c81c747199f70e09c5e024960b010eb5a
ifebrand6 <ifebrand6@gmail.com>
2021-01-27 20:28:17 +0100

    import mood pictures to folder

---
+++ src/Assets/Moods/index.js
@@ -0,0 +1,13 @@
+import Mood1 from './image_21.svg';
+import Mood2 from './image_22.svg';
+import Mood3 from './image_24.svg';
+import Mood4 from './image_25.svg';
+import Mood5 from './image_26.svg';
+import Mood6 from './image_27.svg';
+import Mood7 from './image_28.svg';
+import Mood8 from './image_29.svg';
+import Mood9 from './image_30.svg';
+import Mood10 from './image_31.svg';
+
+
+export {Mood1,Mood2,Mood3,Mood4,Mood5,Mood6,Mood7,Mood8,Mood9,Mood10 } 
