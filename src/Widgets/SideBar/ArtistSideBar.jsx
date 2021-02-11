98f8bc3e9127a3c1708008869d0476ebd8cfa053
sofiree <muosophia0@gmail.com>
2021-02-11 14:42:14 +0100

    FEAT: signed in user and none signed in users have been differentiated

--- src/Widgets/SideBar/ArtistSideBar.jsx
+++ src/Widgets/SideBar/ArtistSideBar.jsx
@@ -1,43 +1,51 @@
-import { ProfilePhoto, UdunkuluLogo } from '../../Assets/Images';
-import { Home,LogOut,Upload } from '../../Assets/Icons';
-import './SideBar.css';
+import { ProfilePhoto, UdunkuluLogo } from "../../Assets/Images";
+import { Home, Upload } from "../../Assets/Icons";
+import "./SideBar.css";
 import { NavLink } from "react-router-dom";
-import { LogOutButton } from '../../Components/Buttons/LogOut';
+import { LogOutButton } from "../../Components/Buttons/LogOut";
 
+const ArtistSidebar = () => {
+  const isLoggedIn = localStorage.getItem("token");
 
+  return (
+      <section>
+        <div class="sidebar" id="sidebar-wrapper">
+          <NavLink to="/" className="nav-link">
+            <img
+              class="sidebar-header"
+              src={UdunkuluLogo}
+              alt="Udunkulu"
+              href="/"
+            />
+          </NavLink>
 
-const ArtistSidebar=()=>{
-    return(
-        <section>
-         <div class="sidebar" id="sidebar-wrapper">
-            <img class="sidebar-header" src={UdunkuluLogo} alt="Udunkulu" href="/" />
-            <ul class="list-group flex-column d-inline-block list-group-sidebar">
-                <li class="list-item">
-                    <NavLink to="/dashboard" className="nav-link">
-                       <button type="button" class="btn btn-dark" id="button-border"><img src={Home} alt=""/></button> 
-                       Home
-                    </NavLink>
-                </li>
+          <ul class="list-group flex-column d-inline-block list-group-sidebar">
+            <li class="list-item">
+              <NavLink to="/dashboard" className="nav-link">
+                <button type="button" class="btn btn-dark" id="button-border">
+                  <img src={Home} alt="" />
+                </button>
+                <p>Home</p>
+              </NavLink>
+            </li>
 
-                <li class="list-item">
-                <NavLink to="/upload-your-music" className="nav-link">
-                    <button type="button" class="btn btn-dark" id="button-border"><img src={Upload} alt=""/></button> 
-                        Upload
-                </NavLink>
-                </li>
-
-            </ul>
-            <ul class="list-group flex-column d-inline-block list-group-sidebar pad-top">
-                <li class="list-item">
-                    {/* TODO replace Udunku logo with dynamic artist img and avatar */}
-                    <img src={ProfilePhoto} alt="" className=" btn profilePhoto"/>
-                    <LogOutButton/>
-                </li>
-            </ul>
-            
+            <li class="list-item">
+              <NavLink to="/upload-your-music" className="nav-link">
+                <button type="button" class="btn btn-dark" id="button-border">
+                  <img src={Upload} alt="" />
+                </button>
+                <p>Upload</p>
+              </NavLink>
+            </li>
+          </ul>
+          <ul class="list-group flex-column d-inline-block list-group-sidebar pad-top">
+            <li class="list-item" id="listItem">
+              {isLoggedIn ? <LogOutButton /> : null}
+            </li>
+          </ul>
         </div>
-        </section>
-    );
-}
+      </section>
+  );
+};
 
-export {ArtistSidebar};
+export { ArtistSidebar };
