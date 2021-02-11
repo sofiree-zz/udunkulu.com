e667ef28e283d98b6a4bf73e772dc13f95813e6d
sofiree <muosophia0@gmail.com>
2021-02-11 16:14:30 +0100

    FEAT: artist homepage UI set

--- src/Pages/App/DashBoard/ArtistHomePage.js
+++ src/Pages/App/DashBoard/ArtistHomePage.js
@@ -1,4 +1,5 @@
 import './ArtistHomePage.css';
+import {Human1, Human2, MusicNotes} from '../../../Assets/Icons';
 
 const ArtistHomePage =(props)=>{
     return(
@@ -9,16 +10,15 @@ const ArtistHomePage =(props)=>{
            
             <div class="row pb-5 innerPadding">
                 <div class="col-sm-4">
-                    <div class="card high card-dashboard">Revenue</div>
-                    <div class="card high card-dashboard">Open Heart</div>
+                    <div class="card high card-dashboard" id="Profile"><p>Profile</p> <img src={Human1} alt=""/></div>
+                    <div class="card high card-dashboard"><p>Open Heart</p></div>
                 </div>
                 <div class="col-sm-4">
-                    <div class="card low card-dashboard">Albums</div>
-                    <div class="card high card-dashboard">Albums</div>
+                    <div class="card low card-dashboard" id="Albums"><p>Albums</p> <img src={MusicNotes} alt=""/></div>
+                    <div class="card high card-dashboard"><p>Revenue</p></div>
                 </div>
                 <div class="col-sm-4">
-                    <div class="card high card-dashboard">Followers</div>
-                    <div class="card low card-dashboard">Profile</div>
+                    <div class="card high card-dashboard" id="Followers"><p>Followers</p> <img src={Human2} alt=""/></div>
                 </div>
             </div>
   
